970a7bed5d40f6d9015b3138ef3b01eccdad9699
Hadriel Tzuk <htzuk@google.com>
2023-05-04 15:33:31 +0300

    Init

--- ops/Jenkinsfile.d/docker.groovy
+++
@@ -1,8 +0,0 @@
-def buildAndUpload(String dockerfilePath) {
-
-    docker.build("${DOCKER_IMAGE}:${env.VERSION}", "-f ${WORKSPACE}/${dockerfilePath} .")
-}
-
-def deleteImage() { sh "docker image rm ${DOCKER_IMAGE}:${env.VERSION}" }
-
-return this
--- ops/Jenkinsfile.d/test.groovy
+++
@@ -1,6 +0,0 @@
-def run(Map params) {
-
-    echo "Running Ferrera"
-}
-
-return this
